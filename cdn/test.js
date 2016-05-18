3d25644c9fbcb497c40b21846fddb1dd47fc9852
Judson Terrell <jterrell@JTerrell-MBP.local>
2016-05-18 10:11:20 -0400

    git ignore

---
+++ cdn/test.js
@@ -0,0 +1,12 @@
+//most basic implementation to instantiate.
+angular.module('widgets', []).directive('testd', function(){
+
+
+    return {
+
+        template: "<div>This is a test</div>"
+
+    }
+
+
+});
